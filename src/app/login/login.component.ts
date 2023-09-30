b245fbbd88e91806fa5c1dd3dad7ebd6df0faaa1
serhankunt <hserhan006@gmail.com>
2023-10-01 02:37:17 +0300

    .

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  userNameOrEmail :string = "";
+  password:string = "";
 
+  signIn(form:NgForm){
+    if(this.userNameOrEmail ===""){
+      alert("Username or email must be valid");
+      return;
+    }
+    if(this.password ===""){
+      alert("Password must be valid");
+      return;
+    }
+    console.log(this.userNameOrEmail);
+    console.log(this.password);
+
+  }
 }
